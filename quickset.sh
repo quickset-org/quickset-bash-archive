f222d08d955f9c6cb9524eaf02040489aedec02e
jaredmdobson <jared.dobson@live.com>
2020-04-11 11:00:06 -0600

    Changes

--- quickset.sh
+++ quickset.sh
@@ -1,41 +1,41 @@
 #!/bin/sh
 
 QUICKSET_VERSION=0.0.0.2;
- 
+QUICKSET_RECIPE=$1;
 
 
 get_os_name_and_version () {
     if [ -f /etc/os-release ]; then
         # freedesktop.org and systemd
         . /etc/os-release
-        OS=$NAME
-        VER=$VERSION_ID
+        QUICKSET_FOUND_OS=$NAME
+        QUICKSET_FOUND_OS_VERSION=$VERSION_ID
     elif type lsb_release >/dev/null 2>&1; then
         # linuxbase.org
-        OS=$(lsb_release -si)
-        VER=$(lsb_release -sr)
+        QUICKSET_FOUND_OS=$(lsb_release -si)
+        QUICKSET_FOUND_OS_VERSION=$(lsb_release -sr)
     elif [ -f /etc/lsb-release ]; then
         # For some versions of Debian/Ubuntu without lsb_release command
         . /etc/lsb-release
-        OS=$DISTRIB_ID
-        VER=$DISTRIB_RELEASE
+        QUICKSET_FOUND_OS=$DISTRIB_ID
+        QUICKSET_FOUND_OS_VERSION=$DISTRIB_RELEASE
     elif [ -f /etc/debian_version ]; then
         # Older Debian/Ubuntu/etc.
-        OS=Debian
-        VER=$(cat /etc/debian_version)
+        QUICKSET_FOUND_OS=Debian
+        QUICKSET_FOUND_OS_VERSION=$(cat /etc/debian_version)
     elif [ -f /etc/SuSe-release ]; then
         # Older SuSE/etc.
         ...
     elif [ -f /etc/redhat-release ]; then
-        # Older Red Hat, CentOS, etc.
+        # Older Red Hat, CentQUICKSET_FOUND_OS, etc.
         ...
     elif sw_vers -productVersion; then
-        OS="macos"
-        VER=${1-:$(sw_vers -productVersion)}
+        QUICKSET_FOUND_OS="macos"
+        QUICKSET_FOUND_OS_VERSION=${1-:$(sw_vers -productVersion)}
     else
         # Fall back to uname, e.g. "Linux <version>", also works for BSD, etc.
-        OS=$(uname -s)
-        VER=$(uname -r)
+        QUICKSET_FOUND_OS=$(uname -s)
+        QUICKSET_FOUND_OS_VERSION=$(uname -r)
     fi
 }
 # From Mikel @ https://unix.stackexchange.com/a/6348
@@ -43,11 +43,13 @@ get_os_name_and_version () {
 
 clear;
 get_os_name_and_version;
+
+
 echo "";
 echo "\e[96m---------------------------------------------------";
 echo "             .__        __                   __   \n  ________ __|__| ____ |  | __  ______ _____/  |_ \n / ____/  |  \  |/ ___\|  |/ / /  ___// __ \   __\\n< <_|  |  |  /  \  \___|    <  \___ \\  ___/|  |  \n \__   |____/|__|\___  >__|_ \/____  >\___  >__|  \n    |__|             \/     \/     \/     \/      ";
 echo "";
-echo "\e[93mquickset \e[94mv$QUICKSET_VERSION\e[96m running for $OS $VER\n";
+echo "\e[93mquickset \e[94mv$QUICKSET_VERSION\e[96m running for $QUICKSET_FOUND_OS $QUICKSET_FOUND_OS_VERSION\n";
 printf "Auto updating quickset for next run...."
 sudo sh -c 'curl -fsSL https://raw.githubusercontent.com/onesupercoder/quickset/master/quickset.sh > /usr/local/bin/quickset';
 printf " Done.\n";
@@ -56,4 +58,7 @@ echo "---------------------------------------------------\e[0m";
 echo " ";
 
 
-echo $1;
+[ "$#" -eq 1 ] || die "Error: Please provide one quickset recipe, $# provided"
+
+QUICKSET_EXACT_RECIPE="$QUICKSET_RECIPE/$QUICKSET_FOUND_OS/$QUICKSET_FOUND_OS_VERSION";
+echo "Attempting quickset for $QUICKSET_EXACT_RECIPE"
