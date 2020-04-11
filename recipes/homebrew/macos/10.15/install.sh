5c0dd8a1e719add24d82c758649b0e0a94953d33
Jared Dobson <jared@dealermade.com>
2020-04-11 17:55:15 -0600

    Changes

--- recipes/homebrew/macos/10.15/install.sh
+++ recipes/homebrew/macos/10.15/install.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -H 'Cache-Control: no-cache' -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+which -s brew;
+if [[ $? != 0 ]] ; then
+    /bin/bash -c "$(curl -H 'Cache-Control: no-cache' -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+    brew update;
+    brew upgrade;
+fi;
+
+
--- recipes/homebrew/ubuntu/18/install.sh
+++ recipes/homebrew/ubuntu/18/install.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
-/bin/bash -c "$(curl -H 'Cache-Control: no-cache' -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+which -s brew;
+if [[ $? != 0 ]] ; then
+    /bin/bash -c "$(curl -H 'Cache-Control: no-cache' -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+else
+    brew update;
+    brew upgrade;
+fi;
 
