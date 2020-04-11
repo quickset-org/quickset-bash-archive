e7c064093c0491bc06486b77b066c7e1b35f169d
Jared Dobson <jared@dealermade.com>
2020-04-11 17:44:57 -0600

    Changes

--- recipes/scala/macos/10.15/install.sh
+++ recipes/scala/macos/10.15/install.sh
@@ -6,6 +6,10 @@ for i in "${QUICKSET_PREREQUISITES[@]}"
 do
    QUICKSET_PREQREQ_REPO_URL="https://raw.githubusercontent.com/onesupercoder/quickset/master/${i}/recipes/macos/10.15/install.sh"
    echo "$QUICKSET_PREQREQ_REPO_URL";
-   /bin/bash -c "$(curl -fsSL "$QUICKSET_PREQREQ_REPO_URL")";
+   /bin/bash --verbose -c "$(curl -fsSL "$QUICKSET_PREQREQ_REPO_URL")";
 done;
 
+printf "* Installing Scala... ";
+sdk install scala 2.13.1
+sdk default scala 2.13.1
+printf " Done.";
