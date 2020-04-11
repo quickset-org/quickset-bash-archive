790ee48c5526d6335a9f93aed10e18b3f4993ee7
jaredmdobson <jared.dobson@live.com>
2020-04-11 13:13:58 -0600

    Changes

--- quickset.sh
+++ quickset.sh
@@ -1,8 +1,9 @@
-#!/bin/sh
+#!/bin/bash
+set -e;
 
 QUICKSET_VERSION=0.0.0.2;
 QUICKSET_RECIPE=$1;
-
+QUICKSET_REPO="https://raw.githubusercontent.com/onesupercoder/quickset/master/";
 
 get_os_name_and_version () {
     if [ -f /etc/os-release ]; then
@@ -46,19 +47,55 @@ get_os_name_and_version;
 
 
 echo "";
-echo "\e[96m---------------------------------------------------";
-echo "             .__        __                   __   \n  ________ __|__| ____ |  | __  ______ _____/  |_ \n / ____/  |  \  |/ ___\|  |/ / /  ___// __ \   __\\n< <_|  |  |  /  \  \___|    <  \___ \\  ___/|  |  \n \__   |____/|__|\___  >__|_ \/____  >\___  >__|  \n    |__|             \/     \/     \/     \/      ";
+echo -e "\e[96m---------------------------------------------------";
+echo -e "             .__        __                   __   \n  ________ __|__| ____ |  | __  ______ _____/  |_ \n / ____/  |  \  |/ ___\|  |/ / /  ___// __ \   __\\n< <_|  |  |  /  \  \___|    <  \___ \\  ___/|  |  \n \__   |____/|__|\___  >__|_ \/____  >\___  >__|  \n    |__|             \/     \/     \/     \/      ";
 echo "";
-echo "\e[93mquickset \e[94mv$QUICKSET_VERSION\e[96m running for $QUICKSET_FOUND_OS $QUICKSET_FOUND_OS_VERSION\n";
+echo -e "\e[93mquickset \e[94mv$QUICKSET_VERSION\e[96m running for $QUICKSET_FOUND_OS $QUICKSET_FOUND_OS_VERSION\n";
 printf "Auto updating quickset for next run...."
-sudo sh -c 'curl -fsSL https://raw.githubusercontent.com/onesupercoder/quickset/master/quickset.sh > /usr/local/bin/quickset';
+
+sudo bash -c "curl -fsSL "${QUICKSET_REPO}"quickset.sh > /usr/local/bin/quickset";
 printf " Done.\n";
 echo " ";
-echo "---------------------------------------------------\e[0m";
+echo -e "---------------------------------------------------\e[0m";
 echo " ";
 
 
 [ "$#" -eq 1 ] || die "Error: Please provide one quickset recipe, $# provided"
 
 QUICKSET_EXACT_RECIPE="$QUICKSET_RECIPE/$QUICKSET_FOUND_OS/$QUICKSET_FOUND_OS_VERSION";
-echo "Attempting quickset for $QUICKSET_EXACT_RECIPE"
+echo "Attempting quickset for $QUICKSET_EXACT_RECIPE";
+
+firstString="I love Suzi and Marry"
+secondString="Sara"
+echo "${firstString/Suzi/$secondString}"    
+
+IFS='.'; # read delimiter
+read -ra QUICKSET_OS_VERSION_ARRAY <<< "$QUICKSET_FOUND_OS_VERSION"; # str is read into an array as tokens separated by IFS
+
+
+for (( i=${#QUICKSET_OS_VERSION_ARRAY[@]}-1; i>=0; i-- ))
+  do QUICKSET_OS_VERSION_ARRAY_SORTED[${#QUICKSET_OS_VERSION_ARRAY_SORTED[@]}]=${QUICKSET_OS_VERSION_ARRAY[i]}
+done;
+
+
+QUICKSET_FOUND_PATH=false
+
+QUICKSET_RECIPE_URL=""
+
+for i in "${QUICKSET_OS_VERSION_ARRAY_SORTED[@]}"
+do
+    if \("$QUICKSET_FOUND_PATH" = false\) -a \(curl --output /dev/null --silent --head --fail "$url"\); then
+        QUICKSET_FOUND_PATH=true
+    else
+        echo "URL does not exist: $url"
+    fi
+   echo "$i"
+   
+   curl -fsSL
+   # or do whatever with individual element of the array
+done
+
+if [ "$QUICKSET_FOUND_PATH" = true ] ; then
+    echo 'Be careful not to fall off!'
+fi
+
--- watchdog.sh
+++ watchdog.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 #sudo apt install entr 
 
