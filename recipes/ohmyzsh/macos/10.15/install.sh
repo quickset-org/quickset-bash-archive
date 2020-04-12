4c9ced2838aa648623bb436b8daad2597e98c96b
Jared Dobson <jared@dealermade.com>
2020-04-11 18:17:51 -0600

    Changes

--- recipes/ohmyzsh/macos/10.15/install.sh
+++ recipes/ohmyzsh/macos/10.15/install.sh
@@ -5,7 +5,6 @@ QUICKSET_PREREQUISITES=( homebrew );
 for i in "${QUICKSET_PREREQUISITES[@]}"
 do
    QUICKSET_PREQREQ_REPO_URL="https://raw.githubusercontent.com/onesupercoder/quickset/master/recipes/${i}/macos/10.15/install.sh"
-   echo "$QUICKSET_PREQREQ_REPO_URL";
    /bin/bash -c "$(curl -H 'Cache-Control: no-cache' -fsSL "$QUICKSET_PREQREQ_REPO_URL")";
 done;
 
